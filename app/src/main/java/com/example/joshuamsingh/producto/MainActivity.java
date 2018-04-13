d8fd9a66351424433f1d44bc42d062d8523b1e98
Joshua M Singh <joshuasingh522@gmail.com>
2018-04-13 13:39:11 +0530

    sadsad

--- app/src/main/java/com/example/joshuamsingh/producto/MainActivity.java
+++ app/src/main/java/com/example/joshuamsingh/producto/MainActivity.java
@@ -151,6 +151,7 @@ public class MainActivity extends AppCompatActivity {
                                 }
 
                                 startActivity(new Intent(MainActivity.this,seller.class));
+                                //sdsad
 
                             }
 
