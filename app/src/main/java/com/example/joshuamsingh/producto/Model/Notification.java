dd4bfbbcfe70b7f899f0ac2a72cb8079d94d4432
Joshua M Singh <joshuasingh522@gmail.com>
2018-04-14 12:38:37 +0530

    s

---
+++ app/src/main/java/com/example/joshuamsingh/producto/Model/Notification.java
@@ -0,0 +1,15 @@
+package com.example.joshuamsingh.producto.Model;
+
+/**
+ * Created by Joshua M Singh on 13-04-2018.
+ */
+
+public class Notification {
+  public String body;
+    public String title;
+
+    public Notification(String body, String title) {
+        this.body = body;
+        this.title = title;
+    }
+}
